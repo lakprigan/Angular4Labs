73aaceae3401b71945009089ff1ca4d1b5308871
Iyer <laxmipriya.iyer@philips.com>
2017-08-02 09:23:52 -0400

    lab 1-3

---
+++ Angular-Workshop/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular-Workshop/src/app/collection/collection.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { IBook } from 'app/ibook';
+
+@Component({
+  selector: 'my-collection',
+  templateUrl: './collection.component.html',
+  styleUrls: ['./collection.component.css']
+})
+export class CollectionComponent implements OnInit {
+
+  constructor() {
+    this.startTime = new Date();
+    this.startTime.setHours(10, 0);
+    this.endTime = new Date();
+    this.endTime.setHours(15, 0);
+  }
+
+  ngOnInit(): void {
+}
+
+  pageTitle:string = 'Books';
+
+    public books:Array<IBook> = 
+    [
+        {
+            id: 1,
+            title: "JavaScript - The Good Parts",
+            author: "Douglas Crockford",
+            isCheckedOut: true,
+            rating: 3
+        },
+        {
+            id: 2,
+            title: "The Wind in the Willows",
+            author: "Kenneth Grahame",
+            isCheckedOut: false,
+            rating: 4
+        },
+        {
+            id: 3,
+            title: "Pillars of the Earth",
+            author: "Ken Follett",
+            isCheckedOut: true,
+            rating: 5
+        },
+        {
+            id: 4,
+            title: "Harry Potter and the Prisoner of Azkaban",
+            author: "J. K. Rowling",
+            isCheckedOut: false,
+            rating: 5
+        }
+    ];
+
+  startTime:Date;
+
+  endTime:Date;
+
+  showOperatingHours:boolean = false;
+}
